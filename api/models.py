73609441c8d01525471ad731475c10d78771a149
uenobu106 <uenobu106@gmail.com>
2021-12-20 20:20:02 +0900

    ✅ add setting fix and model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+  title = models.CharField(max_length=50)
+  create_at = models.DateTimeField(auto_now_add=True)
+  update_at = models.DateTimeField(auto_now=True)
+
+  def __srt__(self):
+    return self.title
--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
+    'api'
 ]
 
 MIDDLEWARE = [
@@ -106,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'ja'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
